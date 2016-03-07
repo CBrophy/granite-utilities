28bf69ee33cce133a82d4a3e5a9c8e810377051e
Charles Brophy <charlesb771@gmail.com>
2016-03-07 14:06:54 -0800

    Add euclidean distance test

--- granite-base/src/test/java/org/granite/math/VectorToolsTest.java
+++ granite-base/src/test/java/org/granite/math/VectorToolsTest.java
@@ -8,8 +8,8 @@ public class VectorToolsTest {
 
     @Test
     public void testDotProduct() throws Exception {
-        double[] vector1 = new double[]{1,2,3};
-        double[] vector2 = new double[]{2,3,4};
+        double[] vector1 = new double[]{1, 2, 3};
+        double[] vector2 = new double[]{2, 3, 4};
 
         double expected = 1.0 * 2.0 + 2.0 * 3.0 + 3.0 * 4.0;
 
@@ -17,8 +17,8 @@ public class VectorToolsTest {
     }
 
     @Test
-    public void testNorm(){
-        double[] vector1 = new double[]{1,2,3};
+    public void testNorm() {
+        double[] vector1 = new double[]{1, 2, 3};
 
         double expected = Math.sqrt(1.0 * 1.0 + 2.0 * 2.0 + 3.0 * 3.0);
 
@@ -26,10 +26,10 @@ public class VectorToolsTest {
     }
 
     @Test
-    public void testCosine(){
+    public void testCosine() {
 
-        double[] vector1 = new double[]{1,2,3};
-        double[] vector2 = new double[]{2,3,4};
+        double[] vector1 = new double[]{1, 2, 3};
+        double[] vector2 = new double[]{2, 3, 4};
         double expectedDotProduct = 1.0 * 2.0 + 2.0 * 3.0 + 3.0 * 4.0;
         double expectedNorm1 = Math.sqrt(1.0 * 1.0 + 2.0 * 2.0 + 3.0 * 3.0);
         double expectedNorm2 = Math.sqrt(2.0 * 2.0 + 3.0 * 3.0 + 4.0 * 4.0);
@@ -38,16 +38,46 @@ public class VectorToolsTest {
     }
 
     @Test
-    public void testDistanceVector(){
-        double[] vector1 = new double[]{1,2,3};
-        double[] vector2 = new double[]{2,3,4};
+    public void testDistanceVector() {
+        double[] vector1 = new double[]{1, 2, 3};
+        double[] vector2 = new double[]{2, 3, 4};
 
-        double[] expected = new double[]{1.0-2.0,2.0-3.0,3.0-4.0};
+        double[] expected = new double[]{1.0 - 2.0, 2.0 - 3.0, 3.0 - 4.0};
 
         double[] distance = VectorTools.distanceVector(vector1, vector2);
 
-        for(int i = 0; i < vector1.length; i++){
-            assertEquals(expected[i], distance[i],0.0);
+        for (int i = 0; i < vector1.length; i++) {
+            assertEquals(expected[i], distance[i], 0.0);
         }
+    }
+
+    @Test
+    public void testEuclideanDistance() {
+        double[] center = new double[]{0, 0};
+
+        double[] v1 = new double[]{1, 1};
+        double[] v2 = new double[]{1, 0};
+        double[] v3 = new double[]{1, -1};
+        double[] v4 = new double[]{0, -1};
+        double[] v5 = new double[]{-1, -1};
+        double[] v6 = new double[]{-1, 0};
+        double[] v7 = new double[]{-1, 1};
+        double[] v8 = new double[]{0, 1};
+
+        assertEquals(hypotenuse(center[0], v1[0], center[1], v1[1]), VectorTools.euclideanDistance(center, v1), 0.01);
+        assertEquals(hypotenuse(center[0], v2[0], center[1], v2[1]), VectorTools.euclideanDistance(center, v2), 0.01);
+        assertEquals(hypotenuse(center[0], v3[0], center[1], v3[1]), VectorTools.euclideanDistance(center, v3), 0.01);
+        assertEquals(hypotenuse(center[0], v4[0], center[1], v4[1]), VectorTools.euclideanDistance(center, v4), 0.01);
+        assertEquals(hypotenuse(center[0], v5[0], center[1], v5[1]), VectorTools.euclideanDistance(center, v5), 0.01);
+        assertEquals(hypotenuse(center[0], v6[0], center[1], v6[1]), VectorTools.euclideanDistance(center, v6), 0.01);
+        assertEquals(hypotenuse(center[0], v7[0], center[1], v7[1]), VectorTools.euclideanDistance(center, v7), 0.01);
+        assertEquals(hypotenuse(center[0], v8[0], center[1], v8[1]), VectorTools.euclideanDistance(center, v8), 0.01);
+
+    }
+
+    private static double hypotenuse(double x1, double x2, double y1, double y2) {
+        // Euclidean distance in two dimensions is the length of the
+        // hypotenuse between the two points
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 }
